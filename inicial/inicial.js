bb8a05f361a86fedb786c10becfec28209667d3d
Diana-souzx <dianasouza.0798@gmail.com>
2025-03-09 09:52:42 -0300

    iniciando projeto

---
+++ inicial/inicial.js
@@ -0,0 +1,95 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    const vermais= document.getElementById('vermais1'); 
+    const abrir = document.getElementById('bolo-de-chocolate');
+    const fechar= document.getElementById('fechar1');
+    
+    vermais.addEventListener('click', function() { 
+        abrir.classList.toggle('open');
+    });
+
+    fechar.addEventListener('click', function(){
+        abrir.classList.remove('open');
+        
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() { 
+    const vermais= document.getElementById('vermais2'); 
+    const abrir = document.getElementById('torta-morango');
+    const fechar= document.getElementById('fechar2');
+    
+    vermais.addEventListener('click', function() { 
+        abrir.classList.toggle('open');
+    });
+
+    fechar.addEventListener('click', function(){
+        abrir.classList.remove('open');
+        
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() { 
+    const vermais= document.getElementById('vermais3'); 
+    const abrir = document.getElementById('cookie');
+    const fechar= document.getElementById('fechar3');
+    
+    vermais.addEventListener('click', function() { 
+        abrir.classList.toggle('open');
+    });
+
+    fechar.addEventListener('click', function(){
+        abrir.classList.remove('open');
+        
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() { 
+    const vermais= document.getElementById('vermais4'); 
+    const abrir = document.getElementById('macaron');
+    const fechar= document.getElementById('fechar4');
+    
+    vermais.addEventListener('click', function() { 
+        abrir.classList.toggle('open');
+    });
+
+    fechar.addEventListener('click', function(){
+        abrir.classList.remove('open');
+        
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() { 
+    const vermais= document.getElementById('vermais5'); 
+    const abrir = document.getElementById('tiramisu');
+    const fechar= document.getElementById('fechar5');
+    
+    vermais.addEventListener('click', function() { 
+        abrir.classList.toggle('open');
+    });
+
+    fechar.addEventListener('click', function(){
+        abrir.classList.remove('open');
+        
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() { 
+    const vermais= document.getElementById('vermais6'); 
+    const abrir = document.getElementById('torta-de-limao');
+    const fechar= document.getElementById('fechar6');
+    
+    vermais.addEventListener('click', function() { 
+        abrir.classList.toggle('open');
+    });
+
+    fechar.addEventListener('click', function(){
+        abrir.classList.remove('open');
+        
+    });
+
+});
---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('log').addEventListener('click', function(){
+    window.location.href = './login/login.html';
+});
+
+document.getElementById('cad').addEventListener('click', function(){
+    window.location.href = './cadastro/cadastro.html';
+});
